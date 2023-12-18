922b037f52effea3b0c55e7509a164f08b5afd93
DLCQQ <vlad.koles100@gmail.com>
2023-12-18 16:03:01 +0300

    add_mystr

---
+++ mystr.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO, encoding='utf-8', filename='mystr_logg.log')
+
+class MyStr(str):
+    def __new__(cls, value, author):
+        obj = super().__new__(cls, value)
+        obj.author = author
+        obj.time = datetime.now().strftime('%Y-%m-%d %H:%M')
+        return obj
+        
+    def __str__(self):
+        return f"{super().__str__()} (Автор: {self.author}, Время создания: {self.time})"
+    
+    def __repr__(self):
+        return f"MyStr({super().__repr__()}, '{self.author}')"
+
+def main():
+    parser = argparse.ArgumentParser(description='Пример запуска из командной строки')
+    parser.add_argument('value', type=str, help='Значение')
+    parser.add_argument('author', type=str, help='Автор')
+    args = parser.parse_args()
+    
+    try:
+        my_str = MyStr(args.value, args.author)
+        print(my_str)
+        logging.info(f"Успешно создан экземпляр MyStr: {my_str}")
+    except Exception as e:
+        logging.error(f"Ошибка при создании экземпляра MyStr: {str(e)}")
+
+if __name__ == '__main__':
+    main()
